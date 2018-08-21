b7e06e83b3efb2560d1d4c6f0a401e92757b2588
agata okolska <agata.okolska@gmail.com>
2018-08-21 10:12:45 +0200

    refactor, deleting " ;"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 
 class App extends React.Component {
@@ -32,4 +32,4 @@ class App extends React.Component {
 
 }
 
-export default App;
+export default App
