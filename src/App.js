13182752250ad83089925612074be498a885c982
agata okolska <agata.okolska@gmail.com>
2018-08-21 10:11:03 +0200

    render email and picture

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 
 
 class App extends React.Component {
+
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=10")
+      .then(response => response.json())
+      .then(dataFromResponse =>
+        this.setState({
+          data: dataFromResponse.results
+        })
+      )
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      this.state.data &&
+      this.state.data.map(user => (
+        <div key={user.login.uuid}>
+          <img alt={"thumbnail"} src={user.picture.thumbnail} />
+          <br /> {user.email}
+          <hr />
+        </div>
+      ))
     );
   }
+
 }
 
 export default App;
